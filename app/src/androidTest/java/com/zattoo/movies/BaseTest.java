9a41820b883ce34b489a617172830f9e87749e7f
sajid.mehmood <sajid.mehmood@idealo.de>
2021-09-19 22:53:42 +0200

    Created BaseTest class

---
+++ app/src/androidTest/java/com/zattoo/movies/BaseTest.java
@@ -0,0 +1,25 @@
+package com.zattoo.movies;
+
+import androidx.test.espresso.IdlingResource;
+import androidx.test.rule.ActivityTestRule;
+
+import com.zattoo.movies.presentation.MainActivity;
+
+import org.junit.Rule;
+
+public class BaseTest extends Util {
+    final int timeout = 10000;
+    private IdlingResource resources;
+
+    public IdlingResource getResources() {
+        return resources;
+    }
+
+    public void setResources(IdlingResource resources) {
+        this.resources = resources;
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+}
