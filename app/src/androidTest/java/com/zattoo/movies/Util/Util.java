af403551cadaa9ba284ccc9a5896994b394d9bdc
sajid.mehmood <sajid.mehmood@idealo.de>
2021-09-20 09:36:09 +0200

    Created util classes

---
+++ app/src/androidTest/java/com/zattoo/movies/Util/Util.java
@@ -0,0 +1,132 @@
+package com.zattoo.movies.Util;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.util.concurrent.TimeoutException;
+
+public class Util extends UtilKotlin {
+
+    /**
+     * Perform action of waiting for a specific view id.
+     * @param viewId The id of the view to wait for.
+     * @param millis The timeout of until when to wait for.
+     */
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
+
+     static Matcher<View> hasValueEqualTo(final String content) {
+
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Has EditText/TextView the value:  " + content);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextView) && !(view instanceof EditText)) {
+                    return false;
+                }
+                if (view != null) {
+                    String text;
+                    if (view instanceof TextView) {
+                        text = ((TextView) view).getText().toString();
+                    } else {
+                        text = ((EditText) view).getText().toString();
+                    }
+
+                    return (text.equalsIgnoreCase(content));
+                }
+                return false;
+            }
+        };
+    }
+
+    public static class ColorMatcher implements Matcher<View> {
+
+        private final int matchColor;
+
+        public ColorMatcher(int matchColor) {
+            this.matchColor = matchColor;
+        }
+
+        @Override
+        public boolean matches(Object item) {
+            Context context = ((View)item).getContext();
+            int c2 = context.getColor(this.matchColor);
+            int c1 = ((ColorDrawable)((View)item).getBackground()).getColor();
+            return c1 == c2;
+        }
+
+        @Override
+        public void describeMismatch(Object item, Description mismatchDescription) {
+
+        }
+
+        @Override
+        public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("with text color: ");
+        }
+    }
+}
