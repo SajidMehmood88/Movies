b06da29aa89e1abb16345ac132ad1bf4956b59ff
sajid.mehmood <sajid.mehmood@idealo.de>
2021-09-20 09:55:23 +0200

    Created SimpleIdlingResources class

---
+++ app/src/main/java/com/zattoo/movies/presentation/SimpleIdlingResource.java
@@ -0,0 +1,37 @@
+package com.zattoo.movies.presentation;
+
+import androidx.annotation.Nullable;
+import androidx.test.espresso.IdlingResource;
+
+import com.bumptech.glide.request.ResourceCallback;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SimpleIdlingResource implements IdlingResource {
+    @Nullable
+    private volatile ResourceCallback mCallback;
+
+    private AtomicBoolean mIsIdleNow = new AtomicBoolean(true);
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        return mIsIdleNow.get();
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback callback) {
+        mCallback = callback;
+    }
+
+    public void setIdleState(boolean isIdleNow) {
+        mIsIdleNow.set(isIdleNow);
+        if (isIdleNow && mCallback != null) {
+            mCallback.onTransitionToIdle();
+        }
+    }
+}
