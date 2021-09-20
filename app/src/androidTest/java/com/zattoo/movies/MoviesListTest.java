605a8d822c480d941f68a07bea5992694cd45d82
sajid.mehmood <sajid.mehmood@idealo.de>
2021-09-20 19:23:31 +0200

    Created movies list test

--- app/src/androidTest/java/com/zattoo/movies/MoviesListTest.java
+++ app/src/androidTest/java/com/zattoo/movies/MoviesListTest.java
@@ -2,26 +2,36 @@ package com.zattoo.movies;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.assertion.ViewAssertions.selectedDescendantsMatch;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.zattoo.movies.tool.Util.withViewAtPosition;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 
+import com.zattoo.movies.tool.Util;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class MoviesListTest {
-
-
+public class MoviesListTest extends BaseTest {
 
     @Test
-    public void checkMoviesList() {
-//        onData(anything()).inAdapterView(withId(R.id.recyclerView)).atPosition(0)
-//                .onChildView(withId(R.id.movies_title)).check(matches(withText(startsWith("Entwinted"))));
-
-//        onView(withId(R.id.recyclerView)).check(doesNotExist());
-//        onView(allOf(withId(R.id.toolbar), withChild(withText("")))).check(matches(isDisplayed()));
-//        onView(ViewMatchers.withId(R.id.recyclerView)).check(matches(isDisplayed()));
-        onView(withId(R.id.recyclerView))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-
+    public void checkMoviesList() throws InterruptedException {
+        Thread.sleep(3000);
+        onView(withId(R.id.recyclerView)).check(new Util.RecyclerViewItemCountAssertion(1));
     }
 }
