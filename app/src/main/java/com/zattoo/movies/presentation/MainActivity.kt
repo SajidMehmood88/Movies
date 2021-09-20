e5f2ca8da4133db134ab9314a0e39b1dd333fdb8
sajid.mehmood <sajid.mehmood@idealo.de>
2021-09-20 09:56:06 +0200

    Added idlingResources to MainActivity.kt

--- app/src/main/java/com/zattoo/movies/presentation/MainActivity.kt
+++ app/src/main/java/com/zattoo/movies/presentation/MainActivity.kt
@@ -2,12 +2,20 @@ package com.zattoo.movies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
+import androidx.annotation.VisibleForTesting
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.zattoo.movies.R
 import com.zattoo.movies.databinding.ActivityMainBinding
+import androidx.test.espresso.IdlingResource
+
+
+@Nullable
+private var mIdlingResource: SimpleIdlingResource? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +27,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    @VisibleForTesting
+    fun getIdlingResource(): IdlingResource {
+        if (mIdlingResource == null) {
+            mIdlingResource = SimpleIdlingResource()
+        }
+        return mIdlingResource as SimpleIdlingResource
     }
 }
